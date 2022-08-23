ffe130bfad18edb9bf6a9d8db62842eeb4e01111
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-08-23 23:19:23 +0600

    header added

--- app.js
+++ app.js
@@ -1,98 +1,98 @@
-const nameArray = [];
- function display(nameList){ 
-    let table = document.getElementById('tb');
-    table.innerHTML = "";
-    if(nameArray.length > 5){
-        alert('invalid Input')
-        for(let i = 0; i < nameList.length -1; i++){
-            const name = nameArray[i].playerName;
+// const nameArray = [];
+//  function display(nameList){ 
+//     let table = document.getElementById('tb');
+//     table.innerHTML = "";
+//     if(nameArray.length > 5){
+//         alert('invalid Input')
+//         for(let i = 0; i < nameList.length -1; i++){
+//             const name = nameArray[i].playerName;
     
-            const tr = document.createElement("tr")
+//             const tr = document.createElement("tr")
     
-            tr.innerHTML = `
-            <td>${i + 1}</td>
-            <td>${name}</td>
-            `;
-            table.appendChild (tr);
+//             tr.innerHTML = `
+//             <td>${i + 1}</td>
+//             <td>${name}</td>
+//             `;
+//             table.appendChild (tr);
 
-        } 
-        return;
-    }
+//         } 
+//         return;
+//     }
     
-    for(let i = 0; i < nameList.length; i++){
-        const name = nameArray[i].playerName;
+//     for(let i = 0; i < nameList.length; i++){
+//         const name = nameArray[i].playerName;
 
-        const tr = document.createElement("tr");
+//         const tr = document.createElement("tr");
 
-        tr.innerHTML = `
-        <td>${i + 1}</td>
-        <td>${name}</td>
-        `;
-        table.appendChild (tr);
+//         tr.innerHTML = `
+//         <td>${i + 1}</td>
+//         <td>${name}</td>
+//         `;
+//         table.appendChild (tr);
 
-    }
+//     }
     
- }
+//  }
 
 
  
-function addToSelect(element){
-    const playerName = element.parentNode.parentNode.children[0].innerText;
+// function addToSelect(element){
+//     const playerName = element.parentNode.parentNode.children[0].innerText;
      
-if(nameArray.length < 5){
-    nameArray.push({playerName});
-    element.setAttribute('disabled', true)
-}
-else{
-    alert('Top Five Football er selected Now. You don,t 5+ player ! Thank You Sir')
-}
-document.getElementById('total-added').innerText = nameArray.length;
+// if(nameArray.length < 5){
+//     nameArray.push({playerName});
+//     element.setAttribute('disabled', true)
+// }
+// else{
+//     alert('Top Five Football er selected Now. You don,t 5+ player ! Thank You Sir')
+// }
+// document.getElementById('total-added').innerText = nameArray.length;
 
-display(nameArray);
+// display(nameArray);
      
-}
-function getInputValue(id){
-    const input = document.getElementById(id);
-    const inputString = input.value;
-    const inputValue = parseInt(inputString);
-    if(Math.sign(inputValue) === -1){
-        alert('Invalid Input')
-    }
-    else{
-        return inputValue; 
-    }
+// }
+// function getInputValue(id){
+//     const input = document.getElementById(id);
+//     const inputString = input.value;
+//     const inputValue = parseInt(inputString);
+//     if(Math.sign(inputValue) === -1){
+//         alert('Invalid Input')
+//     }
+//     else{
+//         return inputValue; 
+//     }
       
-}
+// }
 
-document.getElementById('calculate').addEventListener('click', function(){
+// document.getElementById('calculate').addEventListener('click', function(){
 
-    const perPlayer = getInputValue('per-player');
+//     const perPlayer = getInputValue('per-player');
     
-    const perPlayerTotalCost = perPlayer *nameArray.length;
-    if(isNaN(perPlayer)){
-        alert('Inavlid Input')
-        return;
-    }
-     document.getElementById('player-expense').innerText = perPlayerTotalCost;
+//     const perPlayerTotalCost = perPlayer *nameArray.length;
+//     if(isNaN(perPlayer)){
+//         alert('Inavlid Input')
+//         return;
+//     }
+//      document.getElementById('player-expense').innerText = perPlayerTotalCost;
     
-});
+// });
 
-document.getElementById('calculate-total').addEventListener('click', function(){
-    const player = document.getElementById('player-expense').innerText;
-    const playerExpense = parseInt(player);
-    const managerExpense = getInputValue('manager');
-    const coachExpense = getInputValue('coach');
+// document.getElementById('calculate-total').addEventListener('click', function(){
+//     const player = document.getElementById('player-expense').innerText;
+//     const playerExpense = parseInt(player);
+//     const managerExpense = getInputValue('manager');
+//     const coachExpense = getInputValue('coach');
 
-    const total = playerExpense + managerExpense + coachExpense;
+//     const total = playerExpense + managerExpense + coachExpense;
     
-    const totalCost = document.getElementById('total-cost');
-    if(isNaN(total)){
-        alert('Invalid Input')
-        return ;
-    }
-    totalCost.innerText = total;
+//     const totalCost = document.getElementById('total-cost');
+//     if(isNaN(total)){
+//         alert('Invalid Input')
+//         return ;
+//     }
+//     totalCost.innerText = total;
 
-});
+// });
 
 
 
